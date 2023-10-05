354159b75368454459f6d5750e1c2f07ec4860e0
ohbonsai <letbonsaibe@gmail.com>
2023-10-05 21:09:55 +0800

    debug version

--- src/component/Loader.jsx
+++ src/component/Loader.jsx
@@ -1,3 +1,6 @@
-export default function Loader() {
+import {Html, useProgress} from "@react-three/drei";
 
+export default function Loader() {
+    const {progress} = useProgress()
+    return <Html center>{progress} % loaded</Html>
 }
